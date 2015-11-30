7af4e4a7187ea12812d839380f7809408ebb2d75
Yeechan Lu <wz.bluesnow@gmail.com>
2015-11-30 21:16:23 +0800

    Update to new API

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ var readLocalFile = function(name) {
 
 var qml = ModAPI.QMLFile("Controls/SelfSwitchBox.qml")
 var node = qml.root.node
+
 node.describe = "LabeledEditableComboBox"
-
-var currentCharacter = _.find(node.publicMembers, {name: "currentCharacter"})
-currentCharacter.statement = "editText"
-
-var setCharacter = _.find(node.functions, {name: "setCharacter"})
-setCharacter.content = [
+node.publicMember("currentCharacter").statement = "editText"
+node.object("model", "['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']")
+node.object("onEditTextChanged", [
+  '{',
+  '    var character = editText;',
+  '    if (member.length && DataManager.setObjectValue(object, member, character)) {',
+  '        helper.setModified();',
+  '        modified();',
+  '    }',
+  '}',
+].join("\n"))
+node.function("setCharacter", [
   'function setCharacter(character) {',
   '    for (var i = 0; i < model.length; i++) {',
   '        if (model[i] === character) {',
@@ -25,21 +32,6 @@ setCharacter.content = [
   '    }',
   '    editText = character;',
   '}',
-].join("\n");
-
-var onEditTextChanged = node.newObject()
-onEditTextChanged.name = "onEditTextChanged"
-onEditTextChanged.value = [
-  '{',
-  '    var character = editText;',
-  '    if (member.length && DataManager.setObjectValue(object, member, character)) {',
-  '        helper.setModified();',
-  '        modified();',
-  '    }',
-  '}',
-].join("\n");
-
-var model = _.find(node.objects, {name: "model"})
-model.value = "['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']"
+].join("\n"))
 
 qml.save()
